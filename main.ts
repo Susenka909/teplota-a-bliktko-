aa9c8ffa8dbfe586d8cac846901248d3226caaf6
Susenka909 <116568733+Susenka909@users.noreply.github.com>
2022-11-20 12:33:40 +0100

    Počáteční soubory projektu MakeCode

---
+++ main.ts
@@ -0,0 +1,20 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(input.temperature())
+})
+basic.forever(function () {
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
